f89d354eeed8db3ca81d4d76993cbb76b2ba077a
Mujtaba Karimi <mujtaba.karimi2000@gmail.com>
2023-08-23 16:04:14 +0430

    just a save

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,10 +7,10 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow={"hidden"} >
+    <Card borderRadius={10} overflow={"hidden"}>
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
