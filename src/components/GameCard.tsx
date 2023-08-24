3dd55db6cfdb67b36ee4874f5502e5462f9016b7
Mujtaba Karimi <mujtaba.karimi2000@gmail.com>
2023-08-24 10:06:17 +0430

    just a save

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,7 +12,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
