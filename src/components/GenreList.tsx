931fc4e22c9bfa2adacbd661421564bbbb0dee6f
Mujtaba Karimi <mujtaba.karimi2000@gmail.com>
2023-08-26 11:03:32 +0430

    just a save

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -31,7 +31,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
               src={getCroppedImageUrl(genre.image_background)}
             />
             <Button
-            fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
               fontSize="lg"
               variant="link"
